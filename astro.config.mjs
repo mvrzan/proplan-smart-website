729d2b5abf71016643a28cde55c4d2f3dff08325
mvrzan <mvrzan@icloud.com>
2024-08-07 22:20:25 -0400

    add astro sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
+  site: "https://proplansmart.hr",
   i18n: {
     defaultLocale: "hr_HR",
     locales: ["en", "hr_HR"],
